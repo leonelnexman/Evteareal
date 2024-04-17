6af3b571eb087b7b8b760000133d7efe48d1800a
leonelnexman <reshevdev@gmail.com>
2024-04-17 10:01:20 +0300

    Updates

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,40 +1,31 @@
 export class Modal {
-  constructor() {
-      document.addEventListener('DOMContentLoaded', () => {
-          var closeModalBtn = document.getElementById('closeModalBtn');
-          var modalQuestion = document.getElementById('modal');
-          var body = document.body;
-
-          function openModal() {
-              if (modalQuestion) {
-                  modalQuestion.classList.add('active');
-              }
-              if (body) {
-                  body.classList.add('overflow-hidden');
-              }
-          }
-
-          function closeModal() {
-              if (modalQuestion) {
-                  modalQuestion.classList.remove('active');
-              }
-              if (body) {
-                  body.classList.remove('overflow-hidden');
-              }
-          }
-
-          if (closeModalBtn) {
-              closeModalBtn.addEventListener('click', closeModal);
-          }
-
-          var isFirstVisit = localStorage.getItem('isFirstVisit');
-
-          if (!isFirstVisit) {
-              setTimeout(() => {
-                  openModal();
-                  localStorage.setItem('isFirstVisit', 'true');
-              }, 5000);
-          }
-      });
-  }
+    constructor() {
+        document.addEventListener('DOMContentLoaded', () => {
+            var modal = document.getElementById("myModal");
+            var btn = document.getElementById("openModalBtn");
+            var span = document.getElementsByClassName("close")[0];
+            
+            function openModal() {
+                modal.classList.add("active");
+            }
+            
+            function closeModal() {
+                modal.classList.remove("active");
+            }
+            
+            btn.onclick = function() {
+                openModal();
+            }
+            
+            span.onclick = function() {
+                closeModal();
+            }
+            
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    closeModal();
+                }
+            }
+        });
+    }
 }
